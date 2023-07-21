80996099d72367049fbc59066af41ef4b27b5275
Brendan Dagys <bdagys@rentsync.com>
2023-07-20 20:13:27 -0400

    Make GET request with TCP stream

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+fn main() -> std::io::Result<()> {
+    let host = "www.brendandagys.com:80";
+
+    let mut connection = TcpStream::connect(host)?;
+
+    connection.write_all(b"GET / HTTP/1.0")?;
+    connection.write_all(b"\r\n")?;
+
+    connection.write_all(b"Host: www.brendandagys.com")?;
+    connection.write_all(b"\r\n\r\n")?;
+
+    std::io::copy(&mut connection, &mut std::io::stdout())?;
+
+    Ok(())
+}
